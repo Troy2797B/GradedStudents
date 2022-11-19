5197f6507fbe029dd64df40f3788b2ca9f8c6c10
Troy2797B <112650105+Troy2797B@users.noreply.github.com>
2022-11-19 12:42:08 -0500

    trying to add more things

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,86 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Student {
+    public Double[] testScores;
+    public String firstName;
+    public String lastName;
+
+    ArrayList<Double> examScores;
+
+    public Double examScore;
+
+    public int numberOfExamsTaken;
+
+    public Student(String firstName, String lastName, Double[] testScores) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.testScores = testScores;
+    }
+
+    public Student (String firstName, String lastName, ArrayList<Double> examScores){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = examScores;
+    }
+    public double getExamScore() {
+        return examScore;
+    }
+
+    public void setExamScore(double examScore) {
+        this.examScore = examScore;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getNumberOfExamsTaken() {
+        return numberOfExamsTaken;
+    }
+
+    public String getExamScores() {
+//        String str = "";
+//        for (Double score : testScores) {
+//            str = String.format("%,.1f", score);
+//            return str;
+//        }return str;
+//        String strArray = Arrays.toString(testScores);
+//        StringBuffer sb = new StringBuffer();
+//        for (int i = 0; i < strArray.length(); i++){
+//            sb.append(strArray[i]);
+//        }
+       String results = "";
+        int size = testScores.length;
+        String[] str = new String[size];
+        for(int i=0; i < size; i++) {
+            str[i] = testScores[i].toString();
+            results = results + str[i] + "\n";
+        } return results;
+    }
+
+    public void addExamScore (Double examScore){
+        //turn array into arrayList
+        List<Double> testScoresList = new ArrayList<>(Arrays.asList(testScores));
+        //add the examScore to the end of the arrayList
+        testScoresList.add(examScore);
+        //turn this back into an array
+        //examScores = testScoresList.toArray();
+
+    }
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,37 @@
 package io.zipcoder;
 
-public class StudentTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
+public class StudentTest {
+    String firstName = "Troy";
+    String lastName = "Butler";
+    Double[] testScores = {100.0, 98.6, 104.2};
+    Student troy = new Student(firstName, lastName, testScores);
+    @Test
+    public void testGetExamScores() {
+        //Given: the student
+        String firstName = "Troy";
+        String lastName = "Butler";
+        Double[] testScores = {100.0, 98.6, 104.2};
+        Student troy = new Student(firstName, lastName, testScores);
+        //when: you call this method
+        String expected = "100.0\n98.6\n104.2\n";
+        String actual = troy.getExamScores();
+        //then we get the results
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void addExamScoreTest(){
+        //given: a student and an exam score
+        Double examScore = 100.0;
+        //when: this method is called
+        //troy.addExamScore(examScore);
+        System.out.println(troy.getExamScores());
+        //then the method adds an exam score tot the back of the array
+        //Assert.assertTrue(testScores.contains(100.0));
+    }
 }
